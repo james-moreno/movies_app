fe670bcdbe89e7e46eb320b2d68f5c6418a3b68d
Niko Hosn <niko@fromscratchcs.com>
2016-09-19 10:48:38 -0700

    Niko revisions

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Movies, Actors
 # Create your views here.
+def index(request):
+    people = Actors.objects.get()
+    movies = Movies.objects.get()
+    return render(request, 'movies/index.html')
