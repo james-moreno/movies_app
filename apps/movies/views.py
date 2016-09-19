b808151468c7561d0afee712fe219593232abc67
Christian Kwon <christiankwon@outlook.com>
2016-09-19 10:48:06 -0700

    Some stuff

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Movies, Actors
 
 # Create your views here.
+def index(request):
+
+    return render(request, 'movies/index.html')
+
+def add_movie(request):
+    pass
+
+
+def add_actor(request):
+    pass
+
+
+def view_movie(request):
+    pass
+
+
+def view_actor(request):
+    pass
